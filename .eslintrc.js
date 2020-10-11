195dca66c2c0bad3e23098f05e5efa45fe746b0c
Stéphane Malta e Sousa <maltaesousa@gmail.com>
2020-10-11 19:39:20 +0200

    finish examples and exercices

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,10 +15,12 @@ module.exports = {
     "quotes": "off",
     "no-multiple-empty-lines": "off",
     "max-len": ["error", { "code": 120 }],
-    "comma-dangle": "off"
+    "comma-dangle": "off",
+    "prefer-arrow-callback": "off"
   },
   globals: {
     "$": "readonly",
     "ol": "readonly",
+    "proj4": "readonly",
   },
 };
